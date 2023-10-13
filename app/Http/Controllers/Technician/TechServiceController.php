518c09e50662dcbe0d5acfdc6c26da738a7edb0b
imyoujoey <joeylim016@gmail.com>
2023-10-13 15:38:08 +0800

    technician services index, edit working fine

--- app/Http/Controllers/Technician/TechServiceController.php
+++ app/Http/Controllers/Technician/TechServiceController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers\Technician;
 
-use App\Http\Controllers\Controller;
+use App\Models\Service;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+use App\Http\Controllers\Controller;
 
 class TechServiceController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('technician.service.index');
+        if ($request->ajax()) {
+            $services = Service::select(['serviceID', 'serviceName', 'serviceDesc', 'servicePic', 'serviceEstDuration', 'created_at', 'updated_at']);
+            
+            return DataTables::of($services)->toJson();
+        }
+    
+        return view('technician.service.index'); 
+
     }
 
     /**
@@ -42,18 +51,46 @@ class TechServiceController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Service $service)
     {
-        //
+        return view('technician.service.edit', ['service' => $service]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Service $service)
     {
-        //
+        // Validate the form data
+        $validatedData = $request->validate([
+            'serviceTitle' => 'required',
+            'serviceDesc' => 'required',
+            'estDuration' => 'required',
+            'adminName' => 'required' 
+        ]);
+
+        if($request->hasFile('servicePic')){
+            $imgpath = $request->file('servicePic')->store('services', 'public');
+            $service->servicePic = $imgpath;
+        }
+
+        
+        
+
+        // Update the service's attributes
+        $service->serviceName = $validatedData['serviceTitle'];
+        $service->serviceDesc = $validatedData['serviceDesc'];
+        $service->serviceEstDuration = $validatedData['estDuration'];
+        // $service->userTag = $validatedData['adminName'];
+
+        // Save the changes
+        $service->save();
+
+        // Redirect back with a success message
+        return redirect()->route('technician.service.edit', ['service' => $service])
+        ->with('message', 'Service details updated successfully.');
     }
+
 
     /**
      * Remove the specified resource from storage.
