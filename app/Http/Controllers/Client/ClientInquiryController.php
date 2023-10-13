c3a8e86ca2651816de9ee5fb95e1e35063d66483
imyoujoey <joeylim016@gmail.com>
2023-10-13 12:58:56 +0800

    client show inquiries working

--- app/Http/Controllers/Client/ClientInquiryController.php
+++ app/Http/Controllers/Client/ClientInquiryController.php
@@ -2,16 +2,39 @@
 
 namespace App\Http\Controllers\Client;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Models\Inquiry;
 
 class ClientInquiryController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+
+
+    public function index(Request $request)
     {
+        if ($request->ajax()) {
+            $clientInquiries = DB::table('inquiry')
+            ->select([
+                'inquiry.inquiryID',
+                'inquiry.inquiryMessage',
+                'inquiry.inquiryReply'
+            ])
+            ->where('inquiry.userTag', '=', auth()->user()->userTag)
+            ->get();
+
+            $clientInquiries = $clientInquiries->map(function ($inquiry, $index) {
+                $inquiry->rowNumber = $index + 1;
+                return $inquiry;
+            });
+
+            return DataTables::of($clientInquiries)->toJson();
+        }
+
         return view('client.inquiry.index');
     }
 
@@ -58,8 +81,18 @@ class ClientInquiryController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request)
     {
-        //
+        $selectedIds = $request->input('selectedIds');
+        
+        // Ensure selectedIds is an array and not empty
+        if (!is_array($selectedIds) || count($selectedIds) === 0) {
+            return response()->json(['message' => 'No records selected for deletion.'], 400);
+        }
+    
+        // Delete the selected records from the database
+        Inquiry::whereIn('inquiryID', $selectedIds)->delete();
+    
+        return response()->json(['message' => 'Selected records have been deleted successfully.'], 200);
     }
 }
--- resources/views/client/inquiry/index.blade.php
+++ resources/views/client/inquiry/index.blade.php
@@ -2,4 +2,188 @@
 
   
   <x-navbar/>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-8">
+        <div class="h2 mt-4 font-weight-bold">My Inquiries</div>
+
+        <table class="display cell-border" id="client-appointments-table" style="width: 100%;">
+            <thead>
+                <tr>
+                    <th style="max-width: 20px"></th>
+                    <th class="colvis">ID</th>
+                    <th class="colvis" style="max-width: 30px">No.</th>
+                    <th class="colvis">Inquiry</th>
+                    <th class="colvis">Reply</th>
+                    <th style="max-width: 40px"></th>
+                </tr>
+            </thead>
+        </table>
+      </div>
+    </div>
+
+
+
+    {{-- Submit Inquiry --}}
+    <div class="row">
+
+    </div>
+  </div>
 </x-layout>
+
+{{-- Initialize DataTable --}}
+<script>
+  $(function () {
+      var csrfToken = $('meta[name="csrf-token"]').attr('content');
+      var table = $('#client-appointments-table').DataTable({
+        rowId: 'inquiryID',
+          // language: {
+          //     searchBuilder: {
+          //         button: 'Filter'
+          //     },
+          //     buttons: {
+          //         savedStates: {
+          //             0: 'Saved States',
+          //             _: 'Saved States (%d)'
+          //         }
+          //     }
+          // },
+          serverSide: false,
+          processing: true,
+          pagingType: "simple_numbers",
+          responsive: {
+              details: {
+                  type: 'column',
+                  target: -1,
+                  display: $.fn.dataTable.Responsive.display.modal({
+                      header: function (row) {
+                          var data = row.data();
+                          return 'Details of ' + data.appointmentID;
+                      }
+                  }),
+                  renderer: DataTable.Responsive.renderer.tableAll()
+              },
+          },
+          select: 
+                {
+                    style: 'multi', // Allow multi-row selection
+                    selector: 'td:first-child'
+                },
+          ajax: {
+              url: '{{ route('client.inquiry.index') }}',
+              type: 'GET'
+          },
+          dom: 'Bfrtip',
+          lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
+          buttons: [
+              // 'pageLength',
+              // {
+              //     extend: 'colvis',
+              //     text: 'Show/Hide',
+              //     columns: 'th.colvis',
+              //     columnText: function (dt, idx, title) {
+              //         if (idx == 0) {
+              //             return 'Checkbox';
+              //         } else {
+              //             return title;
+              //         }
+              //     }
+              // },
+              // {
+              //     extend: 'collection',
+              //     text: 'Export',
+              //     buttons: ['excel', {
+              //         extend: 'pdfHtml5',
+              //         orientation: 'landscape',
+              //         pageSize: 'LEGAL'
+              //     }]
+              // },
+              // {
+              //     extend: 'collection',
+              //     text: 'Select',
+              //     buttons: ['selectAll', 'selectNone']
+              // },
+              // {
+              //     extend: 'searchBuilder',
+              //     config: {
+              //         depthLimit: 2,
+              //         columns: [0, 1, 2, 3, 4, 5]
+              //     }
+              // },
+              {
+                    extend: 'spacer',
+                    text: 'Action:'
+              },
+              {
+                  extend: 'selected',
+                  text: 'Delete',
+                  action: function (e, dt, button, config) {
+            var selectedIds = table.rows({selected: true}).ids().toArray();
+            
+    
+    
+            if (selectedIds.length === 0) {
+                alert('No records selected for deletion.');
+                return;
+            }
+    
+            if (confirm('Are you sure you want to delete the selected records?')) {
+                $.ajax({
+                    url: "{{ route('client.inquiry.destroy') }}",
+                    type: "DELETE",
+                    data: { selectedIds: selectedIds },
+                    headers: {
+                    'X-CSRF-TOKEN': csrfToken // Include the CSRF token in the headers
+                    },
+                    success: function (response) {
+                        //alert(response.message);
+                        toastr.success('Selected record(s) have been deleted successfully')
+                        // You can reload the DataTable or update it as needed.
+                        table.ajax.reload();
+                    },
+                    error: function (xhr, status, error) {
+                        alert('Error deleting records: ' + xhr.responseText);
+                    }
+                });
+            }
+        }
+              },
+          ],
+          columnDefs:
+            [{
+                targets:-1,
+                orderable:false,
+                className: 'dtr-control arrow-right',
+               
+            },
+            {
+            target: 1,
+            visible: false,
+            searchable: false
+        },
+          ],
+          columns: [
+              {
+                targets:0,
+                data: null,
+                defaultContent: '',
+                orderable: false, 
+                className: 'select-checkbox'
+              },
+
+              
+              { data: 'inquiryID', name: 'inquiryID' },
+              { data: 'rowNumber', name: 'rowNumber' },
+              { data: 'inquiryMessage', name: 'inquiryMessage' },
+              { data: 'inquiryReply', name: 'inquiryReply' },
+              {
+                  data: null,
+                  defaultContent: '',
+                  orderable: false,
+                  className: 'text-center'
+              }
+          ]
+      });
+  });
+</script>
