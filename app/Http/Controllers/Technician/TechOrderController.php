ac60b7de70a3a7ba49a65b085545bb74f54baab0
imyoujoey <joeylim016@gmail.com>
2023-10-13 15:29:38 +0800

    technician orders working fine

--- app/Http/Controllers/Technician/TechOrderController.php
+++ app/Http/Controllers/Technician/TechOrderController.php
@@ -2,17 +2,52 @@
 
 namespace App\Http\Controllers\Technician;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class TechOrderController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('technician.order.index');
+        if ($request->ajax()) {
+            
+            $orders = DB::table('order')
+                ->select([
+                    'order.orderID',
+                    'client.name as clientName', 
+                    'service.serviceName as serviceName',
+                    'technician.name as assignedTechnician',
+                    'order.deviceType',
+                    'order.hardwareManufacturer',
+                    'order.partNo',
+                    'order.serialNo',
+                    'order.diskCapacity',
+                    'order.capacityRestored',
+                    'order.othersIncluded',
+                    'order.orderStatus',
+                    'order.orderStatusPic',
+                    'order.orderRemarks',
+                    'order.created_at',
+                    'order.updated_at'
+                ])
+                ->leftJoin('users as client', 'order.clientTag', '=','client.userTag')
+                ->leftJoin('service','order.serviceID', '=', 'service.serviceID')
+                ->leftJoin('users as technician', 'order.technicianTag', '=', 'technician.userTag')
+                ->where('order.technicianTag', auth()->user()->userTag)
+                ->get();
+            
+            return DataTables::of($orders)->toJson();
+        }
+    
+        return view('technician.order.index'); 
+
     }
 
     /**
@@ -42,17 +77,71 @@ class TechOrderController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Order $order)
     {
-        //
+        $clients = DB::table('users')->where('role','client')->get();
+        $technicians = DB::table('users')->where('role','technician')->get();
+        $services = DB::table('service')->get();
+
+        // $user = User::findOrFail($id);
+        // return view('user.update', compact('user'));
+        return view('technician.order.edit', ['order' => $order,'clients' => $clients, 'technicians'=> $technicians, 'services'=> $services]);
+
+        
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Order $order)
     {
-        //
+        // Validate the form data
+        $validatedData = $request->validate([
+            'deviceType' => 'required',
+            'manufacturer' => 'nullable',
+            'assignedTechnician' => 'required',
+            'partNo' => 'nullable',
+            'orderStatus' => 'required',
+            'serialNo' => 'nullable',
+            'statusPic' => 'nullable|mimes:jpeg,png|max:10000',
+            'othersIncluded' => 'nullable',
+            'remarks' => 'nullable',
+            'diskCapacity' => 'numeric|nullable',
+            'capacityRestored' => 'numeric|nullable',
+        ]);
+
+        if($request->hasFile('statusPic')){
+            $imgpath = $request->file('statusPic')->store('statusPics', 'public');
+            $order->orderStatusPic = $imgpath;
+
+        }
+
+        
+        
+        $technicianTag = TechOrderController::getUserTagByName($validatedData['assignedTechnician']);
+        
+
+        // Update the service's attributes
+        $order->deviceType = $validatedData['deviceType'];
+        $order->hardwareManufacturer = $validatedData['manufacturer'];
+        $order->technicianTag = $technicianTag;
+        $order->partNo = $validatedData['partNo'];
+        $order->orderStatus = $validatedData['orderStatus'];
+        $order->serialNo = $validatedData['serialNo'];        
+        $order->othersIncluded = $validatedData['othersIncluded'];
+        $order->orderRemarks = $validatedData['remarks'];
+        $order->diskCapacity = $validatedData['diskCapacity'];
+        $order->capacityRestored = $validatedData['capacityRestored'];
+
+        
+        // $service->userTag = $validatedData['adminName'];
+
+        // Save the changes
+        $order->save();
+
+        // Redirect back with a success message
+        return redirect()->route('technician.order.edit', ['order' => $order])
+        ->with('message', 'Order details updated successfully.');
     }
 
     /**
@@ -61,5 +150,17 @@ class TechOrderController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+
+
+    public function getUserTagByName($name)
+    {
+        $user = User::where('name', $name)->first();
+    
+        if ($user) {
+            $userTag = $user->userTag; 
+            return $userTag;
+        } 
     }
 }
---
+++ resources/views/technician/order/index.blade.php
@@ -0,0 +1,294 @@
+<x-layout>
+  <x-navbar/>
+    <div class="container-fluid">
+        <div class="h1 ml-2 mt-4 mb-4 font-weight-bold">Assigned Orders</div>
+    
+      <div style="margin-top: 1rem"></div>
+      <table class="display cell-border" id="orders-table" style="width: 100%;">
+          <thead>
+              <tr>
+                  <th style="max-width: 20px"></th>
+                  <th class="colvis">ID</th>
+                  <th class="colvis">Client Name</th>
+                  <th class="colvis">Service Type</th>
+                  <th class="colvis">Assigned Technician</th>
+                  <th class="colvis">Device Type</th>
+                  <th>Hardware<br>Manufacturer</th>
+                  <th>Part No.</th>
+                  <th>Serial No.</th>
+                  <th class="colvis">Disk Capacity</th>
+                  <th class="colvis">Capacity Restored</th>
+                  <th>Others Included</th>
+                  <th class="colvis">Order Status</th>
+                  <th>Order Status Image</th>
+                  <th>Order Remarks</th>
+                  <th>Created At</th>
+                  <th>Updated At</th>
+                  <th style="max-width: 40px"></th>
+              </tr>
+          </thead>
+      </table>
+    </div>
+  
+  
+  
+  
+  </x-layout>
+  
+  
+  
+  {{-- Initialise Datatable --}}
+  <script>
+      $(function () {
+          var csrfToken = $('meta[name="csrf-token"]').attr('content');
+          var table = $('#orders-table').DataTable({
+              rowId: 'orderID',
+              language: 
+              {
+                  searchBuilder: 
+                  {
+                      button: 'Filter'
+                  
+                  },
+              
+                  buttons:
+                  {
+                      savedStates: 
+                      {
+                          0: 'Saved States',
+                          _: 'Saved States (%d)'
+                      }
+                  }
+  
+              },
+              serverSide: false,
+              processing: true,
+              pagingType: "simple_numbers",
+              responsive:
+              {
+                  details:
+                  {
+                      type:'column',
+                      target: -1,
+                      display: $.fn.dataTable.Responsive.display.modal({
+                          header: function (row) 
+                          {
+                              var data = row.data();
+                              return 'Details of ' + data.orderID;
+                          }
+                      }),
+                  renderer: DataTable.Responsive.renderer.tableAll()
+                  },
+              },
+              select: 
+              {
+                  style: 'multi', // Allow multi-row selection
+                  selector: 'td:first-child'
+              },
+              ajax: 
+              {
+                  url: '{{ route('technician.order.index') }}',
+                  type: 'GET'
+  
+              },
+              dom: 'Bfrtip', // Specify the buttons to display
+              lengthMenu: [ [10, 25, 50, -1], [10, 25, 50, "All"] ], // Options for the length menu
+  
+              buttons: 
+              [{
+                  extend: 'spacer',
+                  text: 'Table Control:'
+              },
+  
+              'pageLength',
+              {
+                  extend: 'savedStatesCreate',
+                  config: 
+                  {
+                    splitSecondaries: [
+                      'renameState',
+                      'removeState',
+                      ''
+                      ]
+                  }
+              },
+              {
+                  extend: 'colvis',
+                  text: 'Show/Hide',
+                  columns:'th.colvis',
+                  columnText: function ( dt, idx, title ) 
+                  {
+                      if(idx == 0 ){
+                          return 'Checkbox';
+                      }else{
+                          return title;
+                      }
+                  }
+              },
+              {
+              extend: 'collection',
+              text: 'Export',
+              buttons: [ 'excel',
+                          {
+                          extend: 'pdfHtml5',
+                          orientation: 'landscape',
+                          pageSize: 'LEGAL'
+                          } 
+                       ]
+              },
+              {
+              extend: 'collection',
+              text: 'Select',
+              buttons: [ 'selectAll', 'selectNone' ]
+              },   
+              {
+              extend: 'searchBuilder',
+              config:
+                  {
+                      depthLimit:2,
+                      columns: [1,2,3,4,5,12]
+                  }
+              },
+              {
+                  extend: 'spacer',
+                  style: 'bar',
+                  text: 'Actions:'
+              },
+              // {
+              // text: 'Add',
+              // action: function ( e, dt, button, config ) {
+              // window.location =  "{{ route('technician.order.create') }}" ;
+              // }
+              // },
+              {
+              extend: 'selectedSingle', // Bind to Selected row
+              text: 'Edit',
+              name: 'edit',
+              action: function (e, dt, button, config) {
+                          // Get the selected data
+                  var selectedData = table.row({ selected: true }).data();
+  
+                  if (selectedData) {
+                              // Redirect to the user details page with the selected user's ID
+                              window.location = "{{ route('technician.order.edit', ['order' => ':orderID']) }}".replace(':orderID', selectedData.orderID);
+                              
+  
+                  }
+              }
+              },
+      //         {
+      //         extend: 'selected', // Bind to Selected row
+      //         text: 'Delete',
+      //         action: function (e, dt, button, config) {
+      //     var selectedIds = table.rows({selected: true}).ids().toArray();
+          
+  
+  
+      //     if (selectedIds.length === 0) {
+      //         alert('No records selected for deletion.');
+      //         return;
+      //     }
+  
+      //     if (confirm('Are you sure you want to delete the selected records?')) {
+      //         $.ajax({
+      //             url: "{{ route('technician.order.destroy') }}",
+      //             type: "DELETE",
+      //             data: { selectedIds: selectedIds },
+      //             headers: {
+      //             'X-CSRF-TOKEN': csrfToken // Include the CSRF token in the headers
+      //             },
+      //             success: function (response) {
+      //                 //alert(response.message);
+      //                 toastr.success('Selected record(s) have been deleted successfully')
+      //                 // You can reload the DataTable or update it as needed.
+      //                 table.ajax.reload();
+      //             },
+      //             error: function (xhr, status, error) {
+      //                 alert('Error deleting records: ' + xhr.responseText);
+      //             }
+      //         });
+      //     }
+      // }
+      //         }
+              
+              
+          ],
+  
+  
+          columnDefs:
+          [{
+              targets:-1,
+              orderable:false,
+              className: 'dtr-control arrow-right',
+             
+          }],
+          columns: 
+          [{
+              targets:0,
+              data: null,
+              defaultContent: '',
+              orderable: false, 
+              className: 'select-checkbox'
+          },
+          { data: 'orderID', name: 'orderID'},
+          { data: 'clientName', name: 'clientName' },
+          { data: 'serviceName', name: 'serviceName' },
+          { data: 'assignedTechnician', name: 'assignedTechnician' },
+          { data: 'deviceType', name: 'deviceType' },
+          { data: 'hardwareManufacturer', name: 'hardwareManufacturer', className:'none' },
+          { data: 'partNo', name: 'partNo', className:'none' },
+          { data: 'serialNo', name: 'serialNo', className:'none'},
+          { data: 'diskCapacity', name: 'diskCapacity' },
+          { data: 'capacityRestored', name: 'capacityRestored' },
+          { data: 'othersIncluded', name: 'othersIncluded', className:'none'},
+          { data: 'orderStatus', name: 'orderStatus' },
+          { data: 'orderStatusPic', name: 'orderStatusPic', className:'none',
+          render: function(data, type, row) {
+          // 'data' here represents the URL of the image
+          // 'type' indicates the rendering type (e.g., 'display', 'filter', 'sort')
+          if (type === 'display') {
+          // Display the image as an <img> element
+          var imageUrl = '{{ asset('storage/') }}' + '/' + data;
+          return '<img src="' + imageUrl + '" alt="Image not available" style="max-width: 100px; max-height: 100px;" />';
+          } else {
+          // For other types, return the raw data
+          return data;
+          } 
+      }           
+          },
+          { data: 'orderRemarks', name: 'orderRemarks', className:'none' },
+          { data: 'created_at', name: 'created_at', className:'none', render: function (data) {
+              return new Date(data).toLocaleString("en-GB"); 
+          }},
+          { data: 'updated_at', name: 'updated_at' , className:'none',render: function (data) {
+              return new Date(data).toLocaleString("en-GB");}},
+              {
+            
+            data: null,
+            defaultContent: '',
+            orderable: false,
+            className: 'text-center'
+        }
+                        
+          ]
+          });
+      });
+  
+  
+      
+  
+  
+  
+      
+  
+  
+      
+  
+  
+  
+      
+  </script>
+
+  
+  
+  
