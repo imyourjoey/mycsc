d7b9a1c025faacc8164f65f66eabb05419dbccb3
imyoujoey <joeylim016@gmail.com>
2023-10-17 16:42:30 +0800

    added inquiry form validation errors

--- resources/views/components/landing/inquiry-form.blade.php
+++ resources/views/components/landing/inquiry-form.blade.php
@@ -2,7 +2,8 @@
 
 
 {{-- inquiry form --}}
-<div class="container form-container">
+<div id="hello"></div>
+<div class="container form-container" id="inquiry-form">
   <div class="col-sm-6">
     <h2 class="text-center">Inquiry Form</h2>
     <p class="text-center"> Please fill out this form. We will get in touch with you shortly.</p>
@@ -13,7 +14,7 @@
         <input type="text" class="form-control" id="name" placeholder="Enter your name" name="inquiryName" value="{{ old('inquiryName') }}">
 
         @error('inquiryName')
-        <p class="text-red-500 text-xs mt-1" >Please enter your name</p>
+        <p class="invalid-feedback" >Please enter your name</p>
         @enderror
 
       </div>
@@ -22,7 +23,7 @@
         <input type="email" class="form-control" id="email" placeholder="Enter your email" name="inquiryContactEmail" value="{{ old('inquiryContactEmail') }}">
 
         @error('inquiryContactEmail')
-        <p class="text-red-500 text-xs mt-1">Please enter a valid email address</p>
+        <p class="invalid-feedback">Please enter a valid email address</p>
         @enderror
 
       </div>
@@ -31,7 +32,7 @@
         <textarea class="form-control" type="text" id="message" rows="5" placeholder="Enter your message" name="inquiryMessage">{{ old('inquiryMessage') }}</textarea>
 
         @error('inquiryMessage')
-        <p class="text-red-500 text-xs mt-1">Please enter your message</p>
+        <p class="invalid-feedback">Please enter your message</p>
         @enderror
       </div>
       <div class="form-group">
@@ -40,3 +41,7 @@
     </form>
   </div>
 </div>
+
+
+
+
