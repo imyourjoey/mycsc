a755f559e2384ba7eca0de1485b1ba83e998737b
Joey Lim <85263304+imyourjoey@users.noreply.github.com>
2024-01-03 16:43:32 +0800

    fixed if no trainign wont got error

--- resources/views/components/landing/training.blade.php
+++ resources/views/components/landing/training.blade.php
@@ -7,37 +7,41 @@
     <div class="col-9">
         <div class="training-accordion-container">
         <div class="accordion" id="accordionExample">
-            @foreach($trainings as $training)
-                <div class="card">
-                    <div class="card-header" id="heading{{ $training->trainingID }}">
-                        <h2 class="mb-0">
-                            <button class="btn btn-link btn-block text-left text-dark" type="button"
-                                data-toggle="collapse" data-target="#collapse{{ $training->trainingID }}"
-                                aria-expanded="false"
-                                aria-controls="collapse{{ $training->trainingID }}">
-                                {{ $training->trainingTitle }}
-                            </button>
-                        </h2>
-                    </div>
-                    <div id="collapse{{ $training->trainingID }}" class="collapse show" aria-labelledby="heading{{ $training->trainingID }}"
-                        data-parent="#accordionExample">
-                        <div class="card-body">
-                            
-                            <div>Description: {{ $training->trainingDesc }}</div>
-                            <div>Starting Date & Time: {{ $training->startDateTime->format('j/m/y h:i A') }}</div>
-    <div>Ending Date & Time: {{ $training->endDateTime->format('j/m/y h:i A') }}</div>
-                            <div>Venue: {{ $training->trainingVenue }}</div>
-                            <div>Capacity: {{ $training->trainingCapacity }}</div>
-                            <div>Instructor: {{ $training->trainerName }}</div>
-                            <div>Registration Deadline: {{ $training->regisDeadline->format('j/m/y h:i A') }}</div>
-                            <div class="mt-2">
-                                <a class="btn btn-primary text-white" href="{{ route('showGuestEnrollment', ['trainingID' => $training->trainingID] )}}" >Enrol Now!</a>
-                              </div> 
-                            
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+
+@if ($trainings)
+@foreach($trainings as $training)
+<div class="card">
+    <div class="card-header" id="heading{{ $training->trainingID }}">
+        <h2 class="mb-0">
+            <button class="btn btn-link btn-block text-left text-dark" type="button"
+                data-toggle="collapse" data-target="#collapse{{ $training->trainingID }}"
+                aria-expanded="false"
+                aria-controls="collapse{{ $training->trainingID }}">
+                {{ $training->trainingTitle }}
+            </button>
+        </h2>
+    </div>
+    <div id="collapse{{ $training->trainingID }}" class="collapse show" aria-labelledby="heading{{ $training->trainingID }}"
+        data-parent="#accordionExample">
+        <div class="card-body">
+            
+            <div>Description: {{ $training->trainingDesc }}</div>
+            <div>Starting Date & Time: {{ $training->startDateTime->format('j/m/y h:i A') }}</div>
+<div>Ending Date & Time: {{ $training->endDateTime->format('j/m/y h:i A') }}</div>
+            <div>Venue: {{ $training->trainingVenue }}</div>
+            <div>Capacity: {{ $training->trainingCapacity }}</div>
+            <div>Instructor: {{ $training->trainerName }}</div>
+            <div>Registration Deadline: {{ $training->regisDeadline->format('j/m/y h:i A') }}</div>
+            <div class="mt-2">
+                <a class="btn btn-primary text-white" href="{{ route('showGuestEnrollment', ['trainingID' => $training->trainingID] )}}" >Enrol Now!</a>
+              </div> 
+            
+        </div>
+    </div>
+</div>
+@endforeach
+@endif
+
         </div>
         <img src="{{ asset('img/training.svg') }}" class="training-image wow animate__animated animate__fadeInRight animate__slow" data-wow-offset="100">
     </div>
