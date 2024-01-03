0b85b4c172d1fc03ad529075f3c7c73c7f5f527e
Joey Lim <85263304+imyourjoey@users.noreply.github.com>
2024-01-03 16:29:24 +0800

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,19 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+            'resources/css/app.css', 
+            'resources/css/announcement.css',
+            'resources/css/appointment.css', 
+            'resources/css/hero.css', 
+            'resources/css/inquiry-form.css', 
+            'resources/css/navbar.css', 
+            'resources/css/star-rating.css', 
+            'resources/css/testimonial.css', 
+            'resources/css/training.css', 
+            'resources/css/who-are-we.css', 
+            'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
